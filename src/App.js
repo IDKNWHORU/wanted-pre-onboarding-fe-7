fc3af41ce14355a403d2249761a6df90a330dd3a
rjh <jh.ra@kisos.net>
2022-10-07 13:48:59 +0900

    add login form
    validation check list
    - email: includes '@'
    - password: min-length : 8

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [validInfo, setValid] = useState({email: false, password: false});
+
+  const validation = form => {
+    const {email, password} = form;
+
+  }
+
+  const validEmail = email => {
+    setValid({email: email.target.value.includes(String.fromCharCode(64)), password: validInfo.password});
+  }
+
+  const validPassword = password => {
+    setValid({email: validInfo.email, password: password.target.value.length >= 8});
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <article>
+          <h1>Login</h1>
+          <main>
+            <form onSubmit={(e)=>{e.preventDefault(); validation(e.target);}}>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" placeholder='@ included email address' onChange={validEmail}/>
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" minLength={8} onChange={validPassword}/>
+              {validInfo.email === true && validInfo.password === true && <button type="submit">Login</button>}
+            </form>
+          </main>
+        </article>
       </header>
     </div>
   );
